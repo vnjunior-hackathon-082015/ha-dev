bd1821814b914af917b572e4743e7ef53f40e184
luule_hackathon <luu.le@dnata.com>
2015-08-28 18:40:37 +0400

    Update all Emirates APIs

--- app/scripts/services/emirates-apis.service.js
+++ app/scripts/services/emirates-apis.service.js
@@ -17,8 +17,12 @@
           getTransferEligibility: getTransferEligibility,
           getArabianAdventureTours: getArabianAdventureTours,
           getAdventureAvailability: getAdventureAvailability,
-          getServiceEligibility: getServiceEligibility
-
+          getServiceEligibility: getServiceEligibility,
+          getHotelPropertyListing: getHotelPropertyListing,
+          getHotelRoomAvailability: getHotelRoomAvailability,
+          getCardOfferByArea: getCardOfferByArea,
+          getCardOffersByCategory: getCardOffersByCategory,
+          getCardOffersByDiscount: getCardOffersByDiscount
         };
 
         //==================== Function declaration ====================
@@ -70,8 +74,67 @@
           };
           return $http(req);
         };
-       
-        
+
+        function getHotelPropertyListing(location){
+          var req = {
+            method: 'GET',
+            url: 'https://ec2-52-18-199-95.eu-west-1.compute.amazonaws.com:8143/listofproperties/1.0/?Location='+location,
+            headers: {
+              "Authorization": 'Bearer 615a3bd4d098cdbaf1508f5a5ce4fa7c',
+              "Accept": 'application/json'
+            }
+          };
+          return $http(req);
+        };
+
+        function getHotelRoomAvailability(start, end, location, noOfPax ){
+          var req = {
+            method: 'GET',
+            url: 'https://ec2-52-18-199-95.eu-west-1.compute.amazonaws.com:8143/roomavailability/1.0/?StartDate='+start+'&EndDate='+end+'&Location='+location+'&NoOfCounts='+noOfPax,
+            headers: {
+              "Authorization": 'Bearer 615a3bd4d098cdbaf1508f5a5ce4fa7c',
+              "Accept": 'application/json'
+            }
+          };
+          return $http(req);
+        };
+
+        function getCardOfferByArea(lat, long, radius){
+          var req = {
+            method: 'GET',
+            url: 'https://ec2-52-18-199-95.eu-west-1.compute.amazonaws.com:8143/offersbyarea/1.0/?Latitude='+lat+'&Longitude='+long+'&Radius='+radius,
+            headers: {
+              "Authorization": 'Bearer 615a3bd4d098cdbaf1508f5a5ce4fa7c',
+              "Accept": 'application/json'
+            }
+          };
+          return $http(req);
+        };
+
+        function getCardOffersByCategory(category){
+          var req = {
+            method: 'GET',
+            url: 'https://ec2-52-18-199-95.eu-west-1.compute.amazonaws.com:8143/offersbycategory/1.0/?Category='+category,
+            headers: {
+              "Authorization": 'Bearer 615a3bd4d098cdbaf1508f5a5ce4fa7c',
+              "Accept": 'application/json'
+            }
+          };
+          return $http(req);
+        };
+
+        function getCardOffersByDiscount(discount){
+          var req = {
+            method: 'GET',
+            url: 'https://ec2-52-18-199-95.eu-west-1.compute.amazonaws.com:8143/offersbydiscount/1.0/?Discount='+discount,
+            headers: {
+              "Authorization": 'Bearer 615a3bd4d098cdbaf1508f5a5ce4fa7c',
+              "Accept": 'application/json'
+            }
+          };
+          return $http(req);
+        };
+
     }
 
 })();
