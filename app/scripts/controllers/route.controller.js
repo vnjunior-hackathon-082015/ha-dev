291e696bff4fdc1ed4003003680bb91958eb3072
vnjunior-ha-082015 <vnjuniorha082015>
2015-08-28 11:32:19 +0400

    Update route management

--- app/scripts/controllers/route.controller.js
+++ app/scripts/controllers/route.controller.js
@@ -7,11 +7,45 @@
 
   angular
     .module('hackathonApp')
-    .controller('RouteController',RouteController);
+    .controller('RouteController', RouteController);
 
   RouteController.$inject = ['$scope', '$mdDialog', '$rootScope'];
 
   function RouteController($scope, $mdDialog, $rootScope){
+    var vm = this,
+      destinationList = [];
+    vm.routesCreated = [];
+    vm.routesJoined = [];
+    vm.createRoute = createRoute;
+    vm.onCommentButton = onCommentButton;
 
+    activate();
+
+    //==================== Function declaration ====================
+    function activate(){
+      $rootScope.activeTab = 'route';
+
+      getRoutesData();
+
+      destinationList = commonShareService.getDestination();
+
+      initRoutesProperty();
+    }
+
+    function getRoutesData(){
+
+    }
+
+    function initRoutesProperty(){
+
+    }
+
+    function createRoute(event){
+
+    }
+
+    function onCommentButton(currentTrip){
+
+    }
   }
 });
