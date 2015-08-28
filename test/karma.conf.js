3311ce97b0c075fd6f194b8d45d8413c40e9844e
vnjunior-ha-082015 <vnjuniorha082015>
2015-08-28 18:36:10 +0400

    Sale in trip management

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -35,9 +35,11 @@ module.exports = function(config) {
       'bower_components/angular-bootstrap/ui-bootstrap-tpls.js',
       'bower_components/v-accordion/dist/v-accordion.js',
       'bower_components/angular-ui-bootstrap-datetimepicker/datetimepicker.js',
-      'bower_components/angular-mocks/angular-mocks.js',
+      'bower_components/lodash/lodash.js',
+      'bower_components/angular-google-maps/dist/angular-google-maps.js',
       'bower_components/angular-aria/angular-aria.js',
       'bower_components/angular-material/angular-material.js',
+      'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       "app/scripts/**/*.js",
       "test/mock/**/*.js",
