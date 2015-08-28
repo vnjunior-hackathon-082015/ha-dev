aaf311eefde89f679100dbaa5e81a373edec24ca
Dat Nguyen <sherlockednguyen@gmail.com>
2015-08-28 11:18:35 +0400

    add static data for routes, users. Add function to get its data in common-share.service.js

---
+++ app/assets/data-routes.js
@@ -0,0 +1,457 @@
+var data_routes = [
+    {
+        "routeId": 1,
+        "avatarURL": "images/avatar/hien_pham.jpg",
+        "title": "Dubai Mall Tour",
+        "description": "laboris esse culpa non voluptate occaecat pariatur irure",
+        "tripImage": "images/dubai-img/dubai-mall-1.jpg",
+        "hostedBy": "Hien Pham",
+        "hostedById": 3,
+        "createDate": "2015-01-12 12:11",
+        "minimumCost": 221,
+        "currency": "USD",
+        "totalJoined": 2,
+        "totalMember": 6,
+        "fromDate": "07:00 09/09/2015",
+        "toDate": "20:00 13/09/2015",
+        "destinations": [
+            {
+                "startDate": "2015-01-25 15:00",
+                "endDate": "2015-01-26 11:00",
+                "locationId": 1
+            },
+            {
+                "startDate": "2015-01-25 15:00",
+                "endDate": "2015-01-26 11:00",
+                "locationId": 2
+            },
+            {
+                "startDate": "2015-01-25 15:00",
+                "endDate": "2015-01-26 11:00",
+                "locationId": 3
+            }
+        ],
+        "comments": [
+            {
+                "fullname": "Dat Nguyen",
+                "avatarURL": "images/avatar/dat_nguyen.jpg",
+                "comment": "It\"s sound good"
+            },
+            {
+                "fullname": "Hien Pham",
+                "avatarURL": "images/avatar/hien_pham.jpg",
+                "comment": "That\"s great"
+            },
+            {
+                "fullname": "Hung Thai",
+                "avatarURL": "images/avatar/hung_thai.jpg",
+                "comment": "We should visit Bur Khalifa first, after that we can go to Dubai mall, they\"re in the same area"
+            },
+            {
+                "fullname": "Luu Le",
+                "avatarURL": "images/avatar/luu_le.jpg",
+                "comment": "I\"ll be on time"
+            },
+            {
+                "fullname": "Anh Diep",
+                "avatarURL": "images/avatar/anh_diep.jpg",
+                "comment": "How many room that we will booking"
+            }
+        ]
+    },
+    {
+        "routeId": 2,
+        "avatarURL": "images/avatar/hung_thai.jpg",
+        "title": "Burj Al Arab Tour",
+        "description": "enim elit nisi duis pariatur nostrud dolore fugiat",
+        "tripImage": "images/dubai-img/burj-al-arab-1.jpg",
+        "hostedBy": "Hung Thai",
+        "hostedById": 4,
+        "createDate": "2015-05-26 03:32",
+        "minimumCost": 322,
+        "currency": "USD",
+        "totalJoined": 3,
+        "totalMember": 10,
+        "fromDate": "07:00 09/09/2015",
+        "toDate": "20:00 13/09/2015",
+        "destinations": [
+            {
+                "startDate": "2015-01-25 15:00",
+                "endDate": "2015-01-26 11:00",
+                "locationId": 5
+            },
+            {
+                "startDate": "2015-01-25 15:00",
+                "endDate": "2015-01-26 11:00",
+                "locationId": 6
+            },
+            {
+                "startDate": "2015-01-25 15:00",
+                "endDate": "2015-01-26 11:00",
+                "locationId": 8
+            },
+            {
+                "startDate": "2015-01-25 15:00",
+                "endDate": "2015-01-26 11:00",
+                "locationId": 7
+            },
+            {
+                "startDate": "2015-01-25 15:00",
+                "endDate": "2015-01-26 11:00",
+                "locationId": 9
+            }
+        ],
+        "comments": [
+            {
+                "fullname": "Dat Nguyen",
+                "avatarURL": "images/avatar/dat_nguyen.jpg",
+                "comment": "It\"s sound good"
+            },
+            {
+                "fullname": "Hien Pham",
+                "avatarURL": "images/avatar/hien_pham.jpg",
+                "comment": "That\"s great"
+            },
+            {
+                "fullname": "Hung Thai",
+                "avatarURL": "images/avatar/hung_thai.jpg",
+                "comment": "We should visit Bur Khalifa first, after that we can go to Dubai mall, they\"re in the same area"
+            },
+            {
+                "fullname": "Luu Le",
+                "avatarURL": "images/avatar/luu_le.jpg",
+                "comment": "I\"ll be on time"
+            },
+            {
+                "fullname": "Anh Diep",
+                "avatarURL": "images/avatar/anh_diep.jpg",
+                "comment": "How many room that we will booking"
+            }
+        ]
+    },
+    {
+        "routeId": 3,
+        "avatarURL": "images/avatar/anh_diep.jpg",
+        "title": "Another Dubai Mall Tour",
+        "description": "ut magna ea ea labore eiusmod consequat excepteur",
+        "tripImage": "images/dubai-img/dubai-mall-1.jpg",
+        "hostedBy": "Anh Diep",
+        "hostedById": 5,
+        "createDate": "2014-10-27 07:31",
+        "minimumCost": 669,
+        "currency": "USD",
+        "totalJoined": 3,
+        "totalMember": 7,
+        "fromDate": "07:00 09/09/2015",
+        "toDate": "20:00 13/09/2015",
+        "destinations": [
+            {
+                "startDate": "2015-01-25 15:00",
+                "endDate": "2015-01-26 11:00",
+                "locationId": 16
+            },
+            {
+                "startDate": "2015-01-25 15:00",
+                "endDate": "2015-01-26 11:00",
+                "locationId": 15
+            },
+            {
+                "startDate": "2015-01-25 15:00",
+                "endDate": "2015-01-26 11:00",
+                "locationId": 13
+            },
+            {
+                "startDate": "2015-01-25 15:00",
+                "endDate": "2015-01-26 11:00",
+                "locationId": 12
+            }
+        ],
+        "comments": [
+            {
+                "fullname": "Dat Nguyen",
+                "avatarURL": "images/avatar/dat_nguyen.jpg",
+                "comment": "It\"s sound good"
+            },
+            {
+                "fullname": "Hien Pham",
+                "avatarURL": "images/avatar/hien_pham.jpg",
+                "comment": "That\"s great"
+            },
+            {
+                "fullname": "Hung Thai",
+                "avatarURL": "images/avatar/hung_thai.jpg",
+                "comment": "We should visit Bur Khalifa first, after that we can go to Dubai mall, they\"re in the same area"
+            },
+            {
+                "fullname": "Luu Le",
+                "avatarURL": "images/avatar/luu_le.jpg",
+                "comment": "I\"ll be on time"
+            },
+            {
+                "fullname": "Anh Diep",
+                "avatarURL": "images/avatar/anh_diep.jpg",
+                "comment": "How many room that we will booking"
+            }
+        ]
+    },
+    {
+        "routeId": 4,
+        "avatarURL": "images/avatar/hung_thai.jpg",
+        "title": "Dubai Marina Tour",
+        "description": "ipsum ex sunt proident tempor sint deserunt elit",
+        "tripImage": "images/dubai-img/marina-yach-1.jpg",
+        "hostedBy": "Hung Thai",
+        "hostedById": 4,
+        "createDate": "2014-02-02 10:15",
+        "minimumCost": 478,
+        "currency": "USD",
+        "totalJoined": 4,
+        "totalMember": 7,
+        "fromDate": "07:00 09/09/2015",
+        "toDate": "20:00 13/09/2015",
+        "destinations": [
+            {
+                "startDate": "2015-01-25 15:00",
+                "endDate": "2015-01-26 11:00",
+                "locationId": 10
+            },
+            {
+                "startDate": "2015-01-25 15:00",
+                "endDate": "2015-01-26 11:00",
+                "locationId": 13
+            },
+            {
+                "startDate": "2015-01-25 15:00",
+                "endDate": "2015-01-26 11:00",
+                "locationId": 12
+            }
+        ],
+        "comments": [
+            {
+                "fullname": "Dat Nguyen",
+                "avatarURL": "images/avatar/dat_nguyen.jpg",
+                "comment": "It\"s sound good"
+            },
+            {
+                "fullname": "Hien Pham",
+                "avatarURL": "images/avatar/hien_pham.jpg",
+                "comment": "That\"s great"
+            },
+            {
+                "fullname": "Hung Thai",
+                "avatarURL": "images/avatar/hung_thai.jpg",
+                "comment": "We should visit Bur Khalifa first, after that we can go to Dubai mall, they\"re in the same area"
+            },
+            {
+                "fullname": "Luu Le",
+                "avatarURL": "images/avatar/luu_le.jpg",
+                "comment": "I\"ll be on time"
+            },
+            {
+                "fullname": "Anh Diep",
+                "avatarURL": "images/avatar/anh_diep.jpg",
+                "comment": "How many room that we will booking"
+            }
+        ]
+    },
+    {
+        "routeId": 5,
+        "avatarURL": "images/avatar/anh_diep.jpg",
+        "title": "Miracle Garden Tour",
+        "description": "fugiat ex quis proident nostrud nulla mollit eu",
+        "tripImage": "images/dubai-img/miracle-garden-1.jpg",
+        "hostedBy": "Anh Diep",
+        "hostedById": 5,
+        "createDate": "2015-05-03 07:43",
+        "minimumCost": 504,
+        "currency": "USD",
+        "totalJoined": 4,
+        "totalMember": 9,
+        "fromDate": "07:00 09/09/2015",
+        "toDate": "20:00 13/09/2015",
+        "destinations": [
+            {
+                "startDate": "2015-01-25 15:00",
+                "endDate": "2015-01-26 11:00",
+                "locationId": 12
+            },
+            {
+                "startDate": "2015-01-25 15:00",
+                "endDate": "2015-01-26 11:00",
+                "locationId": 5
+            }
+        ],
+        "comments": [
+            {
+                "fullname": "Dat Nguyen",
+                "avatarURL": "images/avatar/dat_nguyen.jpg",
+                "comment": "It\"s sound good"
+            },
+            {
+                "fullname": "Hien Pham",
+                "avatarURL": "images/avatar/hien_pham.jpg",
+                "comment": "That\"s great"
+            },
+            {
+                "fullname": "Hung Thai",
+                "avatarURL": "images/avatar/hung_thai.jpg",
+                "comment": "We should visit Bur Khalifa first, after that we can go to Dubai mall, they\"re in the same area"
+            },
+            {
+                "fullname": "Luu Le",
+                "avatarURL": "images/avatar/luu_le.jpg",
+                "comment": "I\"ll be on time"
+            },
+            {
+                "fullname": "Anh Diep",
+                "avatarURL": "images/avatar/anh_diep.jpg",
+                "comment": "How many room that we will booking"
+            }
+        ]
+    },
+    {
+        "routeId": 6,
+        "avatarURL": "images/avatar/anh_diep.jpg",
+        "title": "Ski Dubai Tour",
+        "description": "esse nulla labore ex ex anim occaecat amet",
+        "tripImage": "images/dubai-img/skidubai-1.jpg",
+        "hostedBy": "Anh Diep",
+        "hostedById": 5,
+        "createDate": "2015-05-13 03:58",
+        "minimumCost": 411,
+        "currency": "USD",
+        "totalJoined": 5,
+        "totalMember": 6,
+        "fromDate": "07:00 09/09/2015",
+        "toDate": "20:00 13/09/2015",
+        "destinations": [
+            {
+                "startDate": "2015-01-25 15:00",
+                "endDate": "2015-01-26 11:00",
+                "locationId": 4
+            },
+            {
+                "startDate": "2015-01-25 15:00",
+                "endDate": "2015-01-26 11:00",
+                "locationId": 8
+            },
+            {
+                "startDate": "2015-01-25 15:00",
+                "endDate": "2015-01-26 11:00",
+                "locationId": 12
+            },
+            {
+                "startDate": "2015-01-25 15:00",
+                "endDate": "2015-01-26 11:00",
+                "locationId": 11
+            },
+            {
+                "startDate": "2015-01-25 15:00",
+                "endDate": "2015-01-26 11:00",
+                "locationId": 14
+            }
+        ],
+        "comments": [
+            {
+                "fullname": "Dat Nguyen",
+                "avatarURL": "images/avatar/dat_nguyen.jpg",
+                "comment": "It\"s sound good"
+            },
+            {
+                "fullname": "Hien Pham",
+                "avatarURL": "images/avatar/hien_pham.jpg",
+                "comment": "That\"s great"
+            },
+            {
+                "fullname": "Hung Thai",
+                "avatarURL": "images/avatar/hung_thai.jpg",
+                "comment": "We should visit Bur Khalifa first, after that we can go to Dubai mall, they\"re in the same area"
+            },
+            {
+                "fullname": "Luu Le",
+                "avatarURL": "images/avatar/luu_le.jpg",
+                "comment": "I\"ll be on time"
+            },
+            {
+                "fullname": "Anh Diep",
+                "avatarURL": "images/avatar/anh_diep.jpg",
+                "comment": "How many room that we will booking"
+            }
+        ]
+    },
+    {
+        "routeId": 7,
+        "avatarURL": "images/avatar/hien_pham.jpg",
+        "title": "Dubai Museum Tour",
+        "description": "nostrud elit sint tempor velit occaecat cupidatat adipisicing",
+        "tripImage": "images/dubai-img/dubai-museum-1.jpg",
+        "hostedBy": "Hien Pham",
+        "hostedById": 3,
+        "createDate": "2015-01-25 11:35",
+        "minimumCost": 503,
+        "currency": "USD",
+        "totalJoined": 5,
+        "totalMember": 5,
+        "fromDate": "07:00 09/09/2015",
+        "toDate": "20:00 13/09/2015",
+        "destinations": [
+            {
+                "startDate": "2015-01-25 15:00",
+                "endDate": "2015-01-26 11:00",
+                "locationId": 12
+            },
+            {
+                "startDate": "2015-01-25 15:00",
+                "endDate": "2015-01-26 11:00",
+                "locationId": 6
+            },
+            {
+                "startDate": "2015-01-25 15:00",
+                "endDate": "2015-01-26 11:00",
+                "locationId": 2
+            },
+            {
+                "startDate": "2015-01-25 15:00",
+                "endDate": "2015-01-26 11:00",
+                "locationId": 9
+            },
+            {
+                "startDate": "2015-01-25 15:00",
+                "endDate": "2015-01-26 11:00",
+                "locationId": 14
+            }
+        ],
+        "comments": [
+            {
+                "fullname": "Dat Nguyen",
+                "avatarURL": "images/avatar/dat_nguyen.jpg",
+                "comment": "It\"s sound good"
+            },
+            {
+                "fullname": "Hien Pham",
+                "avatarURL": "images/avatar/hien_pham.jpg",
+                "comment": "That\"s great"
+            },
+            {
+                "fullname": "Hung Thai",
+                "avatarURL": "images/avatar/hung_thai.jpg",
+                "comment": "We should visit Bur Khalifa first, after that we can go to Dubai mall, they\"re in the same area"
+            },
+            {
+                "fullname": "Luu Le",
+                "avatarURL": "images/avatar/luu_le.jpg",
+                "comment": "I\"ll be on time"
+            },
+            {
+                "fullname": "Anh Diep",
+                "avatarURL": "images/avatar/anh_diep.jpg",
+                "comment": "How many room that we will booking"
+            }
+        ]
+    }
+]
+;
+
+var strData_Routes = JSON.stringify(data_routes);
+sessionStorage.routes = strData_Routes;
+
