01225fdc651c6429719b91e2d63d677e3d40b8b4
vnjunior-ha-082015 <vnjuniorha082015>
2015-08-28 11:53:16 +0400

    Update route management ....

--- app/scripts/controllers/route.controller.js
+++ app/scripts/controllers/route.controller.js
@@ -7,11 +7,11 @@
 
   angular
     .module('hackathonApp')
-    .controller('RouteController', RouteController);
+    .controller('RouteController',RouteController);
 
-  RouteController.$inject = ['$scope', '$mdDialog', '$rootScope'];
+  RouteController.$inject = ['commonShareService', '$scope', '$mdDialog', '$rootScope'];
 
-  function RouteController($scope, $mdDialog, $rootScope){
+  function RouteController(commonShareService, $scope, $mdDialog, $rootScope){
     var vm = this,
       destinationList = [];
     vm.routesCreated = [];
@@ -33,19 +33,84 @@
     }
 
     function getRoutesData(){
-
+      vm.currentUserInfo = commonShareService.getLoginInfo();
+      vm.routesData = commonShareService.getRoutes();
+      vm.routesData.reverse();
+      vm.routesData.forEach(function(route){
+        vm.currentUserInfo.routesCreated.forEach(function(routeId){
+          if(route.routeId == routeId){
+            vm.routesCreated.push(route);
+          }
+        });
+        vm.currentUserInfo.routesJoined.forEach(function(routeId){
+          if(route.routeId == routeId){
+            vm.routesJoined.push(route);
+          }
+        });
+      });
+      // vm.selectedRoutes = angular.copy(vm.routesData);
     }
 
     function initRoutesProperty(){
+      for(var k = 0; k < vm.routesCreated.length; k++){
+        for (var i = 0; i < vm.routesCreated[k].destinations.length; i++) {
+          for (var j = 0; j < destinationList.length; j++) {
+            if (vm.routesCreated[k].destinations[i].locationId == destinationList[j].id) {
+              vm.routesCreated[k].destinations[i].photo = "background-image : url('images/dubai-img/" + destinationList[j].photo + "');";
+              vm.routesCreated[k].destinations[i].address = destinationList[j].address;
+              vm.routesCreated[k].destinations[i].description = destinationList[j].description;
+              vm.routesCreated[k].destinations[i].locationName = destinationList[j].destination;
+              break;
+            }
+          }
+        }
+      }
 
+      for(var k = 0; k < vm.routesJoined.length; k++){
+        for (var i = 0; i < vm.routesJoined[k].destinations.length; i++) {
+          for (var j = 0; j < destinationList.length; j++) {
+            if (vm.routesJoined[k].destinations[i].locationId == destinationList[j].id) {
+              vm.routesJoined[k].destinations[i].photo = "background-image : url('images/dubai-img/" + destinationList[j].photo + "');";
+              vm.routesJoined[k].destinations[i].address = destinationList[j].address;
+              vm.routesJoined[k].destinations[i].description = destinationList[j].description;
+              vm.routesJoined[k].destinations[i].locationName = destinationList[j].destination;
+              break;
+            }
+          }
+        }
+      }
     }
 
     function createRoute(event){
-
-    }
+      $mdDialog.show({
+        controller: "CreateRouteController",
+        controllerAs: "vm",
+        templateUrl: 'views/create-route.html',
+        parent: angular.element(document.body),
+        targetEvent: event,
+        clickOutsideToClose:false
+      })
+        .then(function(answer) {
+          //Reload routes
+          getRoutesData();
+          initRoutesProperty();
+        }, function() {
+          $scope.status = 'You cancelled the dialog.';
+        });
+    };
 
     function onCommentButton(currentTrip){
-
+      // var currentTrip = vm.selectedRoutes[index];
+      if(currentTrip.activeComment && currentTrip.activeComment.length > 0){
+        $rootScope.loginInfo = commonShareService.getLoginInfo();
+        currentTrip.comments.push({
+          fullname: $rootScope.loginInfo.fullname,
+          avatarURL: $rootScope.loginInfo.avatarURL,
+          comment: currentTrip.activeComment
+        });
+        currentTrip.activeComment = '';
+      }
     }
   }
-});
+
+})();
